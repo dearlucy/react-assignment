99ecc76a0a41c9fc25e72d82f43f39ccaf4ceeda
Rasmus Back <rasmus.back@gmail.com>
2017-08-23 16:04:25 +0300

    Add IndicatorWidget and update Readme

---
+++ client/IndicatorWidget/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import style from './style.less';
+
+class Indicator extends Component {
+  render() {
+    return <div className={style.container}>Implement me!</div>
+  }
+}
+
+export default Indicator;
